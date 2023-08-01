a440fcdd1ce5914dc112c8cfb1e78c57ea1f8ae4
polarSaunderson <Dominic.saunderson@monash.edu>
2023-08-02 00:08:30 +1000

    initial commit of add_plot_frame

---
+++ R/add_plot_frame.R
@@ -0,0 +1,14 @@
+add_plot_frame <- function(sides = c(1:4), kula = "black") {
+  #' Add a frame around a plot
+  #'
+  #' @param sides vector: Which sides should the frame be added to? Bottom (1),
+  #'   left (2), top (3), and / or right (4). Default is to plot all four.
+  #' @param kula What colour should the frame be?
+  #'
+  #' @export
+
+  # Code -----------------------------------------------------------------------
+  for (ii in sides) {
+    add_axis(ii, labels = rep("", 2), tickLength = 0, axisKula = kula)
+  }
+}
